faedc95283437c6aef014713dba9a83f916b83ad
Kenajcrap <fael-1999@hotmail.com>
2017-12-21 11:01:48 -0200

    modified the example playermodel
    keep in mind that this example will not have any bodygroup or skin options, its just to show how the new code goes
    
    enjoy

--- lua/pointshop/items/playermodels/kleiner.lua
+++ lua/pointshop/items/playermodels/kleiner.lua
@@ -3,19 +3,51 @@ ITEM.Price = 250
 ITEM.Model = 'models/player/kleiner.mdl'
 
 function ITEM:OnEquip(ply, modifications)
+	PrintTable(modifications)
 	if not ply._OldModel then
 		ply._OldModel = ply:GetModel()
 	end
-	
-	timer.Simple(1, function() ply:SetModel(self.Model) end)
+	timer.Simple(1, function() 
+		ply:SetModel(self.Model) 
+		ply:SetupHands()
+		ply:SetSkin(modifications.skin)
+		for k, v in pairs (modifications.group) do
+			ply:SetBodygroup(k , modifications.group[k])
+		end
+	end)
 end
 
 function ITEM:OnHolster(ply)
 	if ply._OldModel then
 		ply:SetModel(ply._OldModel)
+		ply:SetupHands()
 	end
+end
+
+function ITEM:Modify(modifications)
+
+	PS:ShowBodygroupChooser(self, modifications)
+end
+
+
+function ITEM:OnModify(ply, modifications)
+   self:OnHolster(ply)
+   self:OnEquip(ply, modifications)
 end
 
 function ITEM:PlayerSetModel(ply)
 	ply:SetModel(self.Model)
+ 	ply:SetupHands()
+end
+
+function PlayerSetHandsModel( ply, ent )
+
+	local simplemodel = player_manager.TranslateToPlayerModelName( ITEM.Model )
+	local info = player_manager.TranslatePlayerHands( simplemodel )
+	if ( info ) then
+		ent:SetModel( info.model )
+		ent:SetSkin( info.skin )
+		ent:SetBodyGroups( info.body )
+	end
+
 end
